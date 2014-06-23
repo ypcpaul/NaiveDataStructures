f76fb1fb40d9c077df0369a1ea1edeef589ceb36
Paul Cayaco <mives29@gmail.com>
2014-06-23 19:05:53 +0800

    Update isMemberTo - change to isMember.  Also now being used on add and remove functions. Allow empty set instantiation.

--- Set.php
+++ Set.php
@@ -27,17 +27,19 @@ class Set implements \Countable, \IteratorAggregate
      * first element would be used as the set's allowed type.
      */
     public function __construct() {
-        $args = func_get_args();
-        if(is_array($args[0]) && func_num_args() === 1)
-            $this->content = $args[0];
-        else
-            $this->content = $args;
-        //get first element's type.  this would serve as the only type that
-        //this set would accept
-        $this->type = gettype(array_values($this->content)[0]);
-        foreach($this->content as $elem) {
-            if(!$this->checkType($elem)) 
-                throw new \InvalidArgumentException(self::TYPE_ERROR);
+        if(func_num_args() > 0) {
+            $args = func_get_args();
+            if(is_array($args[0]) && func_num_args() === 1)
+                $this->content = $args[0];
+            else
+                $this->content = $args;
+            //get first element's type.  this would serve as the only type that
+            //this set would accept
+            $this->type = gettype(array_values($this->content)[0]);
+            foreach($this->content as $elem) {
+                if(!$this->checkType($elem)) 
+                    throw new \InvalidArgumentException(self::TYPE_ERROR);
+            }
         }
     }
 
@@ -128,10 +130,12 @@ class Set implements \Countable, \IteratorAggregate
      * @return Set
      */
     public function add($element) {
-        if(!$this->checkType($element)) {
-            throw new \InvalidArgumentException(self::TYPE_ERROR);
+        if(count($this->content) > 0) {
+            if(!$this->checkType($element)) {
+                throw new \InvalidArgumentException(self::TYPE_ERROR);
+            }
         }
-        if(!is_int(array_search($element, $this->content)))
+        if(!$this->isMember($element))
             $this->content[] = $element;
         return $this;
     }
@@ -141,7 +145,7 @@ class Set implements \Countable, \IteratorAggregate
      * @return Set
      */ 
     public function remove($element) {
-        $index = array_search($element, $this->content);
+        $index = $this->isMember($element, true);
         if(is_int($index))
             array_splice($this->content, $index, 1);
         return $this;
@@ -149,12 +153,14 @@ class Set implements \Countable, \IteratorAggregate
     
     /**
      * Checks if $element is member of this set.
-     * @return boolean
+     * May return int which is the index of the element in the internal
+     * container.
+     * @return boolean | integer
      */
-    public function isMemberOf($element) {
+    public function isMember($element, $returnIndex = false) {
         $index = array_search($element, $this->content);
-        if(is_int($index))
-            return true;
+        if(is_int($index)) 
+            return $returnIndex ? $index : true;
         return false;
     }
 }
