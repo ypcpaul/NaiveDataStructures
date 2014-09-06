73117d5439e2b1e470d7002a101236ee80534fe8
Paul Cayaco <mives29@gmail.com>
2014-09-06 18:29:22 +0800

    Additional tests

--- tests/SetTest.php
+++ tests/SetTest.php
@@ -5,6 +5,15 @@ use tunalaruan\NaiveDataStructures\Set as Set;
 
 class SetTest extends \PHPUnit_Framework_TestCase
 {
+    protected $set;
+    protected $values;
+
+    public function setup()
+    {
+        $this->set = new Set("boom", "baam", "preee");
+        $this->values = ["boom", "baam", "preee"];
+    }
+
     public function testConstruct() 
     {
         $setA = new Set(1,2,3,4,5);
@@ -22,8 +31,39 @@ class SetTest extends \PHPUnit_Framework_TestCase
 
     public function testCheckType()
     {
-        $set = new Set("boom", "baam", "preee");
-        $this->assertFalse($set->checkType(0));
-        $this->assertTrue($set->checkType("aaaa"));
+        $this->assertFalse($this->set->checkType(0));
+        $this->assertTrue($this->set->checkType("aaaa"));
     }
+
+    public function testGetType()
+    {
+        $this->assertEquals("string", $this->set->getType());
+    }
+
+    public function testCount()
+    {
+        $this->assertEquals(3, count($this->set));
+    }
+
+    public function testIteration()
+    {
+        foreach($this->set as $key=>$val) {
+            $this->assertEquals($val, $this->values[$key]);
+        }
+    }
+
+    public function testGetContent()
+    {
+        $this->assertEquals($this->set->getContent(), $this->values);
+    }
+
+    public function testUnion()
+    {
+        $setB = new Set("derp", "durr", "boom");
+        $setUnion = $setB->union($this->set);
+        $this->assertEquals(["derp", "durr", "boom", "baam", "preee"],
+            $setUnion->getContent());
+    }
+
 }
+
